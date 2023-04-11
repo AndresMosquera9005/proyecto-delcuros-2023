5ac7e54670c1a0aad04dcdc91faf6eb8d9f85217
javier mosquera <andrubjj@gmail.com>
2023-04-11 14:49:23 -0500

    11 de abril de 2023,14:49 3 actualizaciones

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,31 @@
 import React from "react"
+import UserContextProvider from "./context/UserContext"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Pagina404 from "./componentes/Pagina404"
 import App from "./componentes/App"
-import "../src/main.css"
-import Cuadricula from "./componentes/Cuadricula"
 import Home from "./componentes/Home"
-import CriptoPage from "./componentes/cripto/CriptoPage"
 import Perfil from "./componentes/usuarios/Perfil"
-import UserContextProvider from "./context/UserContext"
+import Cuadricula from "./componentes/Cuadricula"
+import CriptoPage from "./componentes/cripto/CriptoPage"
 import Login from "./componentes/usuarios/Login"
+import Pagina404 from "./componentes/Pagina404"
+import "../src/main.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <UserContextProvider>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App />}>
-          <Route index element={<Home />} />
-          <Route path='perfil' element={<Perfil />} />
+        <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="perfil" element={<Perfil />} />
         </Route>
-        <Route path='/criptomonedas' element = {<App />}>
-          <Route index element={<Cuadricula />}/>
-          <Route path=':id' element={<CriptoPage />} />
-        </Route>
-        <Route path='/login' element={<Login />} />
-        <Route path='*' element={<Pagina404 />} />
-      </Routes>   
+      </Routes>
+      <Route path="/criptomonedas" element={<App />}>
+        <Route index element={<Cuadricula />} />
+        <Route path="perfil" element={<CriptoPage />} />
+      </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<Pagina404 />} />
     </BrowserRouter>
   </UserContextProvider>
 )
